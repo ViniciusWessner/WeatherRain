a9613ce66f4fc5686e57413b1c56c1b4317a0966
Vinicius <vini.wessner@gmail.com>
2024-09-11 22:05:43 -0300

    Init project and configuration dependency and hilt

---
+++ app/build.gradle.kts
@@ -0,0 +1,94 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    // Plugins block
+    kotlin("plugin.serialization") version "1.9.21"
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.weatherrain"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.weatherrain"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    // Dependencies block
+    val ktorVersion = "2.3.7"
+    implementation("io.ktor:ktor-client-core:$ktorVersion") //client cor
+    implementation("io.ktor:ktor-client-android:$ktorVersion") // client para android melhora engine para requisicao android
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")  //desserialização da response
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion") //serialization
+    implementation("io.ktor:ktor-client-logging:$ktorVersion") //plugin de log
+
+    //dependencias do hilt
+    implementation("com.google.dagger:hilt-android:2.49")
+    kapt("com.google.dagger:hilt-android-compiler:2.49")
+
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
---
+++ app/src/main/java/com/example/weatherrain/CustomApplication.kt
@@ -0,0 +1,8 @@
+package com.example.weatherrain
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CustomApplication: Application() {
+}
